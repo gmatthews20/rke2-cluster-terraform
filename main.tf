47f1b3209d76a5086481adeb974252c3e8cfcead
George Matthews <george.matthews@stfc.ac.uk>
2026-01-23 10:15:47 +0000

    Fix typo in variable names
    should be var.<name> not vars.<name>

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 module "cluster-network" {
   source = "./modules/network"
 
-  os_auth_url = vars.os_auth_url
-  os_cloud    = vars.os_cloud
+  os_auth_url = var.os_auth_url
+  os_cloud    = var.os_cloud
 }
 
 module "control-plane" {
   source = "./modules/control-plane"
 
-  os_auth_url = vars.os_auth_url
-  os_cloud    = vars.os_cloud
+  os_auth_url = var.os_auth_url
+  os_cloud    = var.os_cloud
   # port = module.cluster-network.ip_addr
   cluster_network = module.cluster-network.cluster-network
   secgroup        = module.cluster-network.control-plane-secgroup
@@ -18,8 +18,8 @@ module "control-plane" {
 module "workers" {
   source = "./modules/nodes"
 
-  os_auth_url     = vars.os_auth_url
-  os_cloud        = vars.os_cloud
+  os_auth_url     = var.os_auth_url
+  os_cloud        = var.os_cloud
   master_server   = module.control-plane.master_server
   token           = module.control-plane.token
   cluster_network = module.cluster-network.cluster-network
